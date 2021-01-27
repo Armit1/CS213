a18acaceaca4711dafe71705e4a729017b4f2794
armit <armit@LAPTOP-EUSQQV7M>
2021-01-27 14:46:12 -0500

    Test commit

--- proj1/src/Date.java
+++ proj1/src/Date.java
@@ -1,22 +1,28 @@
 
-
 public class Date {
-    //the attributes are restricted to those given in the project documents
-    private int year;
-    private int month;
-    private int day;
-    
-    public Date(String date) {
-        String[] dateSegments = date.split("/");
-        this.month = Integer.parseInt(dateSegments[0]);
-        this.day = Integer.parseInt(dateSegments[1]);
-        this.year = Integer.parseInt(dateSegments[2]);
-    }
-    public Date() {
-        
-    }
-    public boolean isValid() {
-        
-    }
+	// the attributes are restricted to those given in the project documents
+	private int year;
+	private int month;
+	private int day;
+
+	public Date(String date) {
+		String[] dateSegments = date.split("/");
+		this.month = Integer.parseInt(dateSegments[0]);
+		this.day = Integer.parseInt(dateSegments[1]);
+		this.year = Integer.parseInt(dateSegments[2]);
+	}
+
+	public Date() {
+
+	}
+
+	public boolean isValid() {
+		if ((month <= 12 && month >= 1)
+		    && (day <= 31 && day >= 1)
+		    && (year <= 2021 && year > 999 && (year + "").length() == 4))
+					return true;
+
+		return false;
+	}
 
 }
